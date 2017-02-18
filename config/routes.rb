c8351e26207c76578385c984a493c7c8e20ca2e4
liutao <lt15196622549@gmail.com>
2017-02-18 21:39:46 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
-    resources :orders
-  end
+    resources :orders do
+       member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+     end
+    end
+  
 
   resources :products do
       member do
